a8aff4b662f7d0dbb977f36d25db2f1c551e9b29
yzqz666 <161851982+yzqz666@users.noreply.github.com>
2025-05-15 13:56:09 +0800

    Update p2037.cpp

--- p2037.cpp
+++ p2037.cpp
@@ -1,38 +1,4 @@
-#include <algorithm>
-#include <bitset>
-#include <complex>
-#include <deque>
-#include <exception>
-#include <fstream>
-#include <functional>
-#include <iomanip>
-#include <ios>
-#include <iosfwd>
-#include <iostream>
-#include <istream>
-#include <iterator>
-#include <limits>
-#include <list>
-#include <locale>
-#include <map>
-#include <memory>
-#include <new>
-#include <numeric>
-#include <ostream>
-#include <queue>
-#include <set>
-#include <sstream>
-#include <stack>
-#include <stdexcept>
-#include <streambuf>
-#include <string>
-#include <typeinfo>
-#include <utility>
-#include <valarray>
-#include <vector>
-
-typedef unsigned long long ull;
-typedef long long ll;
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -107,9 +73,6 @@ void solve(){
 }
 
 int main(){
-	std::ios::sync_with_stdio(false);
-	std::cin.tie(0);
-	std::cout.tie(0);
 	solve();
 	return 0;
 }
